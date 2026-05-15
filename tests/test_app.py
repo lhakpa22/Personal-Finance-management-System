0f168edf56fdf4446ec4f47231a1459e4a5b7907
lhakpa22 <149383977+lhakpa22@users.noreply.github.com>
2026-05-15 19:12:15 +0100

    Update test_app.py
    checking if register and login works properly

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,3 +10,19 @@ def test_home_page():
 
     # Check that home page loads successfully
     assert response.status_code == 200
+
+
+# Test register page loads correctly
+def test_register_page():
+    tester = app.test_client()
+    response = tester.get("/register")
+
+    assert response.status_code == 200
+
+
+# Test login page loads correctly
+def test_login_page():
+    tester = app.test_client()
+    response = tester.get("/login")
+
+    assert response.status_code == 200
