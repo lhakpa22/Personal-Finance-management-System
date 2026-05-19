ba8f94ce3b2223e9502c5db728577decba5e87a0
lhakpa22 <149383977+lhakpa22@users.noreply.github.com>
2026-05-19 11:32:02 +0100

    add
     "Prepared app for Render deployment"

--- app.py
+++ app.py
@@ -358,7 +358,8 @@ def delete_budget(budget_id):
     return redirect(url_for("budgets"))
 
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
